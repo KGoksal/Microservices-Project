7ff3545206a6775f9719cf2f5065f7ab0fbd9318
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2024-07-20 23:14:08 -0500

    Update jenkins-server.tf

--- infrastructure/msp-9-jenkins-server-template/jenkins-server.tf
+++ infrastructure/msp-9-jenkins-server-template/jenkins-server.tf
@@ -4,6 +4,7 @@
 //Jenkins Server will run on Amazon Linux 2023 EC2 Instance with
 //custom security group allowing HTTP(80, 8080) and SSH (22) connections from anywhere.
 
+
 provider "aws" {
   region = var.region
   //  access_key = ""
